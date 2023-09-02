bb4b1c1daa072df621728199d2ba15c1d639ccb5
cherlas <istarxc@gmail.com>
2023-09-02 21:45:48 +0800

    inline title not show in preview mode

--- src/workspace_utils.tsx
+++ src/workspace_utils.tsx
@@ -4,6 +4,6 @@ export function refreshPanes() {
     ReactComponentsPlugin.instance.app.workspace.getLeavesOfType('markdown').forEach(leaf => {
         if (leaf.getViewState().state.mode.includes('preview'))
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            (leaf.view as any).previewMode.rerender(true);
+            (leaf.view as any).previewMode.rerender(false);
     });
 }
